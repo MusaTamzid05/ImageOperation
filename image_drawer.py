9c47c558f41b8c3ef0048d53a0271b0fb3c028aa
Musa <tamzid101@gmail.com>
2018-02-28 00:23:02 +0600

    We are now showing the image.

--- image_drawer.py
+++ image_drawer.py
@@ -1,6 +1,14 @@
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import Tk
+from tkinter import Label
+from tkinter import CENTER
 
+from tkinter import Toplevel
+
+
+from PIL import Image
+from PIL import ImageTk
 
 
 def is_valid_image(path):
@@ -19,5 +27,19 @@ def drawer_callback():
         messagebox.showerror(title = "Wrong image" , message = "Image not supported.")
         return
 
-    print("found {}".format(input_file))
+
+    cordinates = []
+
+    def get_image_cordinates(image_path):
+
+        root = Toplevel()
+        root.title("Click two points")
+        #image = PhotoImage(file = image_path)
+
+        image = ImageTk.PhotoImage(Image.open(image_path))
+        w1 = Label(root , compound = CENTER, image = image).pack()
+
+        root.mainloop()
+
+    get_image_cordinates(input_file)
 
