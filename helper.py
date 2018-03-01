9a84be634f26851e82a57a903c6869d49ef760ca
Musa <tamzid101@gmail.com>
2018-03-01 12:44:10 +0600

    code has been refactor.

--- helper.py
+++ helper.py
@@ -1,4 +1,20 @@
 
+'''
+All the function here are common to all operation such as  cropping or drawing.
+All the operation needs two points from the image.Here we have all the code
+that can take two points.
+'''
+
+from tkinter import filedialog
+from tkinter import messagebox
+
+
+from tkinter import Toplevel
+from tkinter import Label
+from tkinter import CENTER
+
+from PIL import Image
+from PIL import ImageTk
 
 def get_new_name(image_path , operation):
 
@@ -7,3 +23,80 @@ def get_new_name(image_path , operation):
 
     return name +  "_" + operation + "." + ext
 
+
+def is_valid_image(path):
+
+    valid_ext = [ "jpg" , "png" , "jpg" ]
+    image_ext = path.split(".")[-1]
+
+    return image_ext in valid_ext
+
+
+def ask_user_for_path():
+
+
+    input_file =  filedialog.askopenfilename()
+
+    if is_valid_image(input_file) == False:
+        messagebox.showerror(title = "Wrong image" , message = "Image not supported.")
+        return None
+
+    return input_file
+
+
+
+
+def callback(operation_function):
+
+    '''
+    The call back will be called by the all the gui callbacks.The arguments it takes
+    is function that will do the operation such as cropping.Because we need
+    the image cordinates for this, we will use this callback to get the image cordinates.
+    Once we have the coordinates that we will call the operatin function.
+    '''
+
+    input_file = ask_user_for_path()
+
+    if input_file is None:
+        return
+
+
+    cordinates = []
+
+
+    def execute_operation(image_path):
+
+        root = Toplevel()
+        root.title("Click two points")
+
+
+        def mouse_handler(event):
+
+            cordinates.append((event.x , event.y))
+
+            if len(cordinates) == 2:
+                operation_function(image_path, cordinates)
+                root.destroy()
+
+
+
+        root.bind("<Button-1>" , mouse_handler)
+        image_data =  Image.open(image_path)
+        image = ImageTk.PhotoImage(image_data)
+        Label(root , compound = CENTER, image = image).pack()
+
+        root.mainloop()
+
+
+    execute_operation(input_file)
+
+def load_image(image_path):
+
+
+    try:
+        image = Image.open(image_path)
+        return image
+
+    except FileNotFoundError as e:
+        messagebox.showerror(title = "Wrong image" , message = "{} path is not a valid image path.".format(image_path))
+        return None
--- image_drawer.py
+++ image_drawer.py
@@ -1,81 +1,23 @@
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import Label
-from tkinter import CENTER
-
-
-from tkinter import Toplevel
-
-
-from PIL import Image
-from PIL import ImageTk
 from PIL import ImageDraw
 
 from helper import get_new_name
+from helper import callback
+from helper import load_image
 
 
-'''
-If you want to change the color of the rectangle
-check the draw_rect_on function and change the
-outline paremeter in ImageDraw inside it.
-'''
-def is_valid_image(path):
-
-    valid_ext = [ "jpg" , "png" , "jpg" ]
-    image_ext = path.split(".")[-1]
-
-    return image_ext in valid_ext
-
+from tkinter import messagebox
 
 def drawer_callback():
 
-    input_file =  filedialog.askopenfilename()
-
-    if is_valid_image(input_file) == False:
-        messagebox.showerror(title = "Wrong image" , message = "Image not supported.")
-        return
-
-
-    cordinates = []
-
-
-    def draw_image_cordinates(image_path):
-
-        root = Toplevel()
-        root.title("Click two points")
-
-
-        def mouse_handler(event):
-
-            cordinates.append((event.x , event.y))
-
-            if len(cordinates) == 2:
-                draw_rect_on(image_path, cordinates)
-                root.destroy()
-
-
-
-        root.bind("<Button-1>" , mouse_handler)
-        image_data =  Image.open(image_path)
-        image = ImageTk.PhotoImage(image_data)
-        Label(root , compound = CENTER, image = image).pack()
-
-        root.mainloop()
-
-
-    draw_image_cordinates(input_file)
-
-
+    callback(draw_rect_on)
 
 def draw_rect_on(image_path , cordinates):
 
-    try:
+    image = load_image(image_path)
 
-        image = Image.open(image_path)
-
-    except FileNotFoundError as e:
-        messagebox.showerror(title = "Wrong image" , message = "{} path is not a valid image path.".format(image_path))
+    if image is None:
         return
+
 
     x1 , y1 = cordinates[0][0] , cordinates[0][1]
     x2, y2=  cordinates[1][0] , cordinates[1][1]
